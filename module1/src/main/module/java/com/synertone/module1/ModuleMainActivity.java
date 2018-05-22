80e4ba7ac726f793416518c760b60020a5479976
yuxinabc <752420765@qq.com>
2018-05-22 17:52:37 +0800

    add new project

---
+++ module1/src/main/module/java/com/synertone/module1/ModuleMainActivity.java
@@ -0,0 +1,12 @@
+package com.synertone.module1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ModuleMainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ module2/src/main/java/com/synertone/module2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.synertone.module2;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
